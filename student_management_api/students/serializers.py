af560dc11512cbef2fe7623dca41c5e24020c254
darshit.h <darshithirani007@gmail.com>
2025-10-28 13:52:05 +0530

    Remove students app

--- student_management_api/students/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import StudentProfile
-
-class StudentProfileSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
-
-    class Meta:
-        model = StudentProfile
-        fields = '__all__'
